a006e65db0f2c007736f13d150cec6d9a36d4935
Cañabano_Dindo <dindor.canabano@gmail.com>
2026-04-22 22:42:51 -1200

    Update 1_🏠_Home.py

--- portfolio_app/pages/1_🏠_Home.py
+++ portfolio_app/pages/1_🏠_Home.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import base64
+import os
 
 
 st.set_page_config(page_title="Home | Dindo", page_icon="🏠", layout="wide")
 
 def get_img_base64(path):
-    with open(path, "rb") as f:
+    full_path = os.path.join(os.path.dirname(__file__), path)
+    with open(full_path, "rb") as f:
         return base64.b64encode(f.read()).decode()
 
-img = get_img_base64("assests/me.png")
+img = get_img_base64("assets/me.png")
 
 st.markdown("""
 <style>
